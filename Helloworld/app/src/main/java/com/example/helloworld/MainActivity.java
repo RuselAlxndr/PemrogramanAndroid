753d063373b1d5a8e6a9d92e32925dcd01ed224c
RuselAlxndr <Rusel.alexander@ti.ukdw.ac.id>
2020-10-08 19:40:07 +0700

    master commit

--- Helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,39 @@
-package com.example.helloworld;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.helloworld.MainActivity2;
+import com.example.helloworld.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText txtUsername;
+    private EditText txtPassword;
+    private Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnlogin = findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                if(txtPassword.getText().toString().equals("admin") && txtUsername.getText().toString().equals("admin")){
+                    Intent i = new Intent( MainActivity.this, MainActivity2.class);
+                    Toast.makeText(getApplicationContext(), "selamat datang "+ txtUsername.getText(),   Toast.LENGTH_LONG).show();
+                    startActivity(i);
+                }else{
+                    Toast.makeText(getApplicationContext(),"gagal "+ txtUsername.getText(), Toast.LENGTH_LONG).show();
+                }}
+        });
+    }}
+
